240168765bdd453c5b9dc63f38386824bea8357f
Alexey Kott <alexey.kott@gmail.com>
2018-12-21 19:31:53 +0300

    ...

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import re
 
 from aioftp import ClientSession
 
-
 cfg = configparser.ConfigParser(inline_comment_prefixes="#")
 cfg.read('config.cfg')
 
@@ -16,21 +15,28 @@ DIR = cfg.get('FTP', 'DIR')
 
 
 async def main():
-
     prefixes = "shutterstock fotolia depositphoto istockphoto".split(' ')
     async with ClientSession(HOST, PORT, USER, PASS) as ftp_session:
         await ftp_session.change_directory(DIR)
-        for path, info in (await ftp_session.list(recursive=True)):
+        for filename, info in (await ftp_session.list(recursive=True)):
             if info['type'] == 'dir':
                 continue
 
-            print(path)
-            print(info, end='\n\n')
+            # print(filename)
+            # print(info, end='\n\n')
 
-            file_filter_regex = re.compile(r'^(?P<dir>[\-ld])(?P<permission>([\-r][\-w][\-xs]){3})\s+(?P<filecode>\d+)\s+(?P<owner>\w+)\s+(?P<group>\w+)\s+(?P<size>\d+)\s+(?P<timestamp>((?P<month>\w{3})\s+(?P<day>\d{1,2})\s+(?P<hour>\d{1,2}):(?P<minute>\d{2}))|((?P<month2>\w{3})\s+(?P<day2>\d{1,2})\s+(?P<year>\d{4})))\s+(?P<name>.+)$', re.MULTILINE)
-            if re.match(file_filter_regex, path):
-                pass
+            pattern = re.compile(r'^(?P<dir>[\-ld])'
+                                 r'(?P<permission>([\-r][\-w][\-xs]){3})\s+'
+                                 r'(?P<filecode>\d+)\s+'
+                                 r'(?P<owner>\w+)\s+'
+                                 r'(?P<group>\w+)\s+'
+                                 r'(?P<size>\d+)\s+'
+                                 r'(?P<timestamp>((?P<month>\w{3})\s+(?P<day>\d{1,2})\s+(?P<hour>\d{1,2}):(?P<minute>\d{2}))|((?P<month2>\w{3})\s+(?P<day2>\d{1,2})\s+(?P<year>\d{4})))\s+'
+                                 r'(?P<name>.+)$', re.MULTILINE)
 
+            prefix = pattern.search(filename)
+
+            print(prefix)
 
 
 if __name__ == "__main__":
